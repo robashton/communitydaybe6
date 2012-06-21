0faf3b77f687071319d417767506458bbce3934b
Rob Ashton <robashton@codeofrob.com>
2012-06-21 11:21:35 +0200

    Service bus

--- server.js
+++ server.js
@@ -1,26 +1,81 @@
 var http = require('http')
 ,   paperboy = require('paperboy')
 ,   socketio = require('socket.io')
+,   azure = require('azure')
 
 
-var server = http.createServer(function(req, res) {
-  paperboy
-    .deliver('site', req, res)
-    .otherwise(function() {
-      res.writeHead({ 'Content-Type' : 'text/plain'})
-      res.end('404 not found lol')
+var queueService = null
+,   server = null
+,   io = null
+
+function startHttp(cb) {
+  server = http.createServer(function(req, res) {
+    paperboy
+      .deliver('site', req, res)
+      .otherwise(function() {
+        res.writeHead({ 'Content-Type' : 'text/plain'})
+        res.end('404 not found lol')
+      })
+  })
+  server.listen(process.env.port || 8000)
+  cb()
+}
+
+function startSockets(cb) {
+  io = socketio.listen(server)
+  io.set('transports', [
+        , 'xhr-polling'
+        , 'jsonp-polling'
+          ])
+
+  io.on('connection', function(socket) {
+    socket.on('command', function(data) {
+      var msg = {
+        body: JSON.stringify(data) 
+      }
+      queueService.sendTopicMessage('commands', msg, function(err) {
+        console.log('sent', err)
+      })
     })
-})
+  })
+  cb()
+}
 
-server.listen(process.env.port || 8000)
-var io = socketio.listen(server)
-io.set('transports', [
-      , 'xhr-polling'
-      , 'jsonp-polling'
-        ]);
+function startServiceBus(cb) {
+  process.env.AZURE_SERVICEBUS_NAMESPACE = 'communityday';
+  process.env.AZURE_SERVICEBUS_ACCESS_KEY = 'xcmDM8w6gztKHLPOvIXq80iTIdXGT7tQxcH7e6VIEpU=';
+  queueService = azure.createServiceBusService()
+  queueService.createTopicIfNotExists('commands', function() {
+    queueService.createSubscription('commands', 'clientj3', function(err) {
+      cb()
+    })
+  })
+}
 
-io.on('connection', function(socket) {
-  socket.on('command', function(data) {
-    socket.broadcast.emit('command', data)
+function startListeningOnBus(cb) {
+  nextMessage()
+  cb()
+}
+
+function nextMessage() {
+  queueService.receiveSubscriptionMessage('commands', 'clientj3', function(err, msg) {
+    if(err) {
+      console.error(err)
+      return nextMessage()
+    }
+    if(msg.body !== '') 
+      io.sockets.emit('command', JSON.parse(msg.body))
+    nextMessage()
+  })
+}
+
+startServiceBus(function() {
+  startHttp(function() {
+    startSockets(function() {
+      startListeningOnBus(function() {
+        console.log('Server started')      
+      })
+    })
   })
 })
+

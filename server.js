56931db2f68201aa77706efb160dac6cb0a303d7
Rob Ashton <robashton@codeofrob.com>
2012-06-21 09:18:51 +0200

    Commited actual site

---
+++ server.js
@@ -0,0 +1,23 @@
+var http = require('http')
+,   paperboy = require('paperboy')
+,   socketio = require('socket.io')
+
+
+var server = http.createServer(function(req, res) {
+  paperboy
+    .deliver('site', req, res)
+    .otherwise(function() {
+      res.writeHead({ 'Content-Type' : 'text/plain'})
+      res.end('404 not found lol')
+    })
+})
+
+server.listen(process.env.port || 8000)
+var io = socketio.listen(server)
+
+
+io.on('connection', function(socket) {
+  socket.on('command', function(data) {
+    socket.broadcast.emit('command', data)
+  })
+})
